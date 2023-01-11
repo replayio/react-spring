847c197f4ac88d967203d829678e8c6395b8da4f
Jaril <jarilvalenciano@gmail.com>
2023-01-11 07:01:56 -0800

    Install replay plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,9 @@
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
+const cypressReplay = require("@replayio/cypress")
 
 module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config)
+  cypressReplay.default(on, config);
+  
   return config
 }
